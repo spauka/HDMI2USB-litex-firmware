d48a3db4fee491c1848173b9442bd0a5f711b866
Tim 'mithro' Ansell <mithro@mithis.com>
2017-08-05 11:00:49 +1000

    More fixing of Travis + Git weirdness.

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -5,11 +5,57 @@ set -e
 echo ""
 echo ""
 echo ""
-echo "- Fetching non shallow to get git version"
+echo "============================================="
+echo "Fixing Travis and Git stuff"
+echo "============================================="
+
+if [ z"$TRAVIS_PULL_REQUEST_SLUG" != z ]; then
+	echo ""
+	echo ""
+	echo ""
+	echo "- Adding pull request source repo"
+	echo "---------------------------------------------"
+	git remote add source https://github.com/$TRAVIS_PULL_REQUEST_SLUG
+	echo "---------------------------------------------"
+
+	echo ""
+	echo ""
+	echo ""
+	echo "- Fetching pull request head"
+	echo "---------------------------------------------"
+	git fetch origin refs/pull/$TRAVIS_PULL_REQUEST/head:pull-head
+	echo "---------------------------------------------"
+	git log -n 5 --graph pull-head
+	echo "---------------------------------------------"
+
+	echo ""
+	echo ""
+	echo ""
+	echo "- Fetching pull request merge"
+	echo "---------------------------------------------"
+	git fetch origin refs/pull/$TRAVIS_PULL_REQUEST/merge:merge-head
+	echo "---------------------------------------------"
+	git log -n 5 --graph merge-head
+	echo "---------------------------------------------"
+fi
+
+echo ""
+echo ""
+echo ""
+echo "- Fetching non-shallow"
 echo "---------------------------------------------"
-git fetch --unshallow && git fetch --tags
+git fetch --unshallow
+
+echo "- Fetching tags"
+echo "---------------------------------------------"
+git fetch --tags --all
+
+
 if [ z"$TRAVIS_BRANCH" != z ]; then
 	TRAVIS_COMMIT_ACTUAL=$(git log --pretty=format:'%H' -n 1)
+	echo ""
+	echo ""
+	echo ""
 	echo "Fixing detached head (current $TRAVIS_COMMIT_ACTUAL -> $TRAVIS_COMMIT)"
 	echo "---------------------------------------------"
 	git log -n 5 --graph
@@ -22,9 +68,12 @@ if [ z"$TRAVIS_BRANCH" != z ]; then
 	git branch -D $TRAVIS_BRANCH || true
 	git checkout $TRAVIS_COMMIT -b $TRAVIS_BRANCH
 	git branch -v
+	echo
 fi
-GIT_REVISION=`git describe`
+
 echo "============================================="
+
+GIT_REVISION=$(git describe)
 
 set -x
 
