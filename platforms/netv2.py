de1bdfbdcab6d601c574250d6f65f349fee699bb
bunnie <bunnie@kosagi.com>
2017-12-14 13:17:39 +0800

    modified pin mapping for netv2 to match netv2mvp

--- platforms/netv2.py
+++ platforms/netv2.py
@@ -3,86 +3,100 @@ from litex.build.xilinx import XilinxPlatform, VivadoProgrammer
 
 _io = [
     # U11F
-    ("clk50", 0, Pins("L19"), IOStandard("LVCMOS33")),
+    ("clk50", 0, Pins("J19"), IOStandard("LVCMOS33")),
 
-    ("user_led", 0, Pins("G22"), IOStandard("LVCMOS33")),
+    ("user_led", 0, Pins("M21"), IOStandard("LVCMOS33")),
 
     ("serial", 0,
-        Subsignal("tx", Pins("H22")), # MCU_RX
-        Subsignal("rx", Pins("J22")), # MCU_TX
+        Subsignal("tx", Pins("E14")), # MCU_RX
+        Subsignal("rx", Pins("E13")), # MCU_TX
         IOStandard("LVCMOS33"),
     ),
 
     ("ddram", 0,
         Subsignal("a", Pins(
-            "AB2 AB3 AB1 AA1 V3 U3 Y1 W1 Y2",
-            "W2 V2 U2 U1 T1"),
+            "U6 V4 W5 V5 AA1 Y2 AB1 AB3 AB2 Y3 W6 Y1 V2 AA3"
+            ),
             IOStandard("SSTL15")),
-        Subsignal("ba", Pins("AA5 AA3 Y3"), IOStandard("SSTL15")),
-        Subsignal("ras_n", Pins("AB5"), IOStandard("SSTL15")),
-        Subsignal("cas_n", Pins("Y4"), IOStandard("SSTL15")),
-        Subsignal("we_n", Pins("AA4"), IOStandard("SSTL15")),
-        Subsignal("dm", Pins("B1 K1 M3 N4"), IOStandard("SSTL15")),
+        Subsignal("ba", Pins("U5 W4 V7"), IOStandard("SSTL15")),
+        Subsignal("ras_n", Pins("Y9"), IOStandard("SSTL15")),
+        Subsignal("cas_n", Pins("Y7"), IOStandard("SSTL15")),
+        Subsignal("we_n", Pins("V8"), IOStandard("SSTL15")),
+        Subsignal("dm", Pins("G1 H4 M5 L3"), IOStandard("SSTL15")),
         Subsignal("dq", Pins(
-            "A1 C2 B2 E2 D2 G1 F1 F3 "
-            "J1 H2 G2 J5 H5 H3 G3 H4 "
-            "K4 J4 L3 K3 M2 K6 J6 L5 "
-            "R1 P1 P2 N2 M6 M5 P6 N5 "
+            "C2 F1 B1 F3 A1 D2 B2 E2 "
+            "J5 H3 K1 H2 J1 G2 H5 G3 "
+            "N2 M6 P1 N5 P2 N4 R1 P6 "
+            "K3 M2 K4 M3 J6 L5 J4 K6 "
             ),
             IOStandard("SSTL15"),
             Misc("IN_TERM=UNTUNED_SPLIT_50")),
-        Subsignal("dqs_p", Pins("E1 K2 M1 P5"), IOStandard("DIFF_SSTL15")),
-        Subsignal("dqs_n", Pins("D1 J2 L1 P4"), IOStandard("DIFF_SSTL15")),
+        Subsignal("dqs_p", Pins("E1 K2 P5 M1"), IOStandard("DIFF_SSTL15")),
+        Subsignal("dqs_n", Pins("D1 J2 P4 L1"), IOStandard("DIFF_SSTL15")),
         Subsignal("clk_p", Pins("R3"), IOStandard("DIFF_SSTL15")),
         Subsignal("clk_n", Pins("R2"), IOStandard("DIFF_SSTL15")),
-        Subsignal("cke", Pins("W6"), IOStandard("SSTL15")),
-        Subsignal("odt", Pins("W5"), IOStandard("SSTL15")),
-        Subsignal("reset_n", Pins("G4"), IOStandard("LVCMOS15")),
-        Subsignal("cs_n", Pins("V4"), IOStandard("SSTL15")),
+        Subsignal("cke", Pins("Y8"), IOStandard("SSTL15")),
+        Subsignal("odt", Pins("W9"), IOStandard("SSTL15")),
+        Subsignal("reset_n", Pins("AB5"), IOStandard("LVCMOS15")),
+        Subsignal("cs_n", Pins("V9"), IOStandard("SSTL15")),
         Misc("SLEW=FAST"),
     ),
 
     ("hdmi_in", 0,
-        Subsignal("clk_p", Pins("Y18"), IOStandard("TMDS_33")),
-        Subsignal("clk_n", Pins("Y19"), IOStandard("TMDS_33")),
-        Subsignal("data0_p", Pins("AB21"), IOStandard("TMDS_33")),
-        Subsignal("data0_n", Pins("AB22"), IOStandard("TMDS_33")),
-        Subsignal("data1_p", Pins("AA19"), IOStandard("TMDS_33")),
-        Subsignal("data1_n", Pins("AB20"), IOStandard("TMDS_33")),
-        Subsignal("data2_p", Pins("AA18"), IOStandard("TMDS_33")),
-        Subsignal("data2_n", Pins("AB18"), IOStandard("TMDS_33")),
-        Subsignal("scl", Pins("L20"), IOStandard("LVCMOS33")), 
-        Subsignal("sda", Pins("H20"), IOStandard("LVCMOS33")), 
+        Subsignal("clk_p", Pins("L19"), IOStandard("TMDS_33")),
+        Subsignal("clk_n", Pins("L20"), IOStandard("TMDS_33")),
+        Subsignal("data0_p", Pins("K21"), IOStandard("TMDS_33")),
+        Subsignal("data0_n", Pins("K22"), IOStandard("TMDS_33")),
+        Subsignal("data1_p", Pins("J20"), IOStandard("TMDS_33")),
+        Subsignal("data1_n", Pins("J21"), IOStandard("TMDS_33")),
+        Subsignal("data2_p", Pins("J22"), IOStandard("TMDS_33")),
+        Subsignal("data2_n", Pins("H22"), IOStandard("TMDS_33")),
+        Subsignal("scl", Pins("T18"), IOStandard("LVCMOS33")), 
+        Subsignal("sda", Pins("V18"), IOStandard("LVCMOS33")), 
     ),
 
     ("hdmi_ov", 0,
-        Subsignal("clk_p", Pins("J20"), IOStandard("TMDS_33")),
-        Subsignal("clk_n", Pins("J21"), IOStandard("TMDS_33")),
-        Subsignal("data0_p", Pins("K21"), IOStandard("TMDS_33")),
-        Subsignal("data0_n", Pins("K22"), IOStandard("TMDS_33")),
-        Subsignal("data1_p", Pins("M21"), IOStandard("TMDS_33")),
-        Subsignal("data1_n", Pins("L21"), IOStandard("TMDS_33")),
-        Subsignal("data2_p", Pins("N22"), IOStandard("TMDS_33")),
-        Subsignal("data2_n", Pins("M22"), IOStandard("TMDS_33")),
-        Subsignal("scl", Pins("G17"), IOStandard("LVCMOS33")), 
-        Subsignal("sda", Pins("H17"), IOStandard("LVCMOS33")), 
+        Subsignal("clk_p", Pins("Y18"), IOStandard("TMDS_33")),
+        Subsignal("clk_n", Pins("Y19"), IOStandard("TMDS_33")),
+        Subsignal("data0_p", Pins("AA18"), IOStandard("TMDS_33")),
+        Subsignal("data0_n", Pins("AB18"), IOStandard("TMDS_33")),
+        Subsignal("data1_p", Pins("AA19"), IOStandard("TMDS_33")),
+        Subsignal("data1_n", Pins("AB20"), IOStandard("TMDS_33")),
+        Subsignal("data2_p", Pins("AB21"), IOStandard("TMDS_33")),
+        Subsignal("data2_n", Pins("AB22"), IOStandard("TMDS_33")),
+        Subsignal("scl", Pins("W17"), IOStandard("LVCMOS33")), 
+        Subsignal("sda", Pins("R17"), IOStandard("LVCMOS33")), 
     ),
 
+# TX0
     ("hdmi_out", 0,
         Subsignal("clk_p", Pins("W19"), IOStandard("TMDS_33")),
         Subsignal("clk_n", Pins("W20"), IOStandard("TMDS_33")),
-        Subsignal("data0_p", Pins("T21"), IOStandard("TMDS_33")),
-        Subsignal("data0_n", Pins("U21"), IOStandard("TMDS_33")),
-        Subsignal("data1_p", Pins("U22"), IOStandard("TMDS_33")),
-        Subsignal("data1_n", Pins("V22"), IOStandard("TMDS_33")),
-        Subsignal("data2_p", Pins("W21"), IOStandard("TMDS_33")),
-        Subsignal("data2_n", Pins("W22"), IOStandard("TMDS_33")),
+        Subsignal("data0_p", Pins("W21"), IOStandard("TMDS_33")),
+        Subsignal("data0_n", Pins("W22"), IOStandard("TMDS_33")),
+        Subsignal("data1_p", Pins("U20"), IOStandard("TMDS_33")),
+        Subsignal("data1_n", Pins("V20"), IOStandard("TMDS_33")),
+        Subsignal("data2_p", Pins("T21"), IOStandard("TMDS_33")),
+        Subsignal("data2_n", Pins("U21"), IOStandard("TMDS_33")),
         IOStandard("TMDS_33")
     ),
 
+# TX1
+#    ("hdmi_out", 0,
+#        Subsignal("clk_p", Pins("G21"), IOStandard("TMDS_33")),
+#        Subsignal("clk_n", Pins("G22"), IOStandard("TMDS_33")),
+#        Subsignal("data0_p", Pins("E22"), IOStandard("TMDS_33")),
+#        Subsignal("data0_n", Pins("D22"), IOStandard("TMDS_33")),
+#        Subsignal("data1_p", Pins("C22"), IOStandard("TMDS_33")),
+#        Subsignal("data1_n", Pins("B22"), IOStandard("TMDS_33")),
+#        Subsignal("data2_p", Pins("B21"), IOStandard("TMDS_33")),
+#        Subsignal("data2_n", Pins("A21"), IOStandard("TMDS_33")),
+#        IOStandard("TMDS_33")
+#    ),
+
     ("hdmi_sda_over_up", 0, Pins("G20"), IOStandard("LVCMOS33")),
-    ("hdmi_sda_over_dn", 0, Pins("H20"), IOStandard("LVCMOS33")),
-    ("hdmi_hdp_over", 0, Pins("H22"), IOStandard("LVCMOS33")),
+    ("hdmi_sda_over_dn", 0, Pins("F20"), IOStandard("LVCMOS33")),
+    ("hdmi_hdp_over", 0, Pins("M22"), IOStandard("LVCMOS33")),
 
 ]
 
@@ -107,7 +121,7 @@ class Platform(XilinxPlatform):
     spiflash_model = "n25q128"
 
     def __init__(self, toolchain="vivado", programmer="vivado"):
-        XilinxPlatform.__init__(self, "xc7a15t-fgg484-2", _io,
+        XilinxPlatform.__init__(self, "xc7a35t-fgg484-2", _io,
                                 toolchain=toolchain)
 
         self.add_platform_command(
